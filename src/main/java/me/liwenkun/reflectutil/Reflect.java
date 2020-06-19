a598838ff03de2513b539cb86fb2b2521c18d596
lwenkun <lwenkun666@gmail.com>
2020-06-20 02:30:05 +0800

    change the name of the param

--- src/main/java/me/liwenkun/reflectutil/Reflect.java
+++ src/main/java/me/liwenkun/reflectutil/Reflect.java
@@ -3,19 +3,19 @@ package me.liwenkun.reflectutil;
 public class Reflect {
 
     // 静态开始的表达式执行
-    public static <T> T withStatic(Class<?> cls, String path, boolean recursive, Object... params) throws Exception {
+    public static <T> T withStatic(Class<?> cls, String expression, boolean recursive, Object... params) throws Exception {
         // noinspection unchecked
-        return (T) Expression.create(cls, path, recursive, params).evaluate(null, params);
+        return (T) Expression.create(cls, expression, recursive, params).evaluate(null, params);
     }
 
-    public static <T> T withStatic(String cls, String path, boolean recursive, Object... params) throws Exception {
-        return withStatic(Class.forName(cls), path, recursive, params);
+    public static <T> T withStatic(String cls, String expression, boolean recursive, Object... params) throws Exception {
+        return withStatic(Class.forName(cls), expression, recursive, params);
     }
 
     // 实例开始的表达式执行
-    public static <T> T withInstance(Object instance, String path, boolean recursive, Object... params) throws Exception {
+    public static <T> T withInstance(Object instance, String expression, boolean recursive, Object... params) throws Exception {
         // noinspection unchecked
-        return (T) Expression.create(instance.getClass(), path, recursive, params).evaluate(instance, params);
+        return (T) Expression.create(instance.getClass(), expression, recursive, params).evaluate(instance, params);
     }
 
 }
