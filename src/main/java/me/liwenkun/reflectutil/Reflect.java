af46797cad5a99a4dd9fa3acf0e10a40fd2c200f
lwenkun <lwenkun666@gmail.com>
2020-06-20 03:01:03 +0800

    small fix: exception handle

--- src/main/java/me/liwenkun/reflectutil/Reflect.java
+++ src/main/java/me/liwenkun/reflectutil/Reflect.java
@@ -2,18 +2,25 @@ package me.liwenkun.reflectutil;
 
 public class Reflect {
 
-    // 静态开始的表达式执行
-    public static <T> T withStatic(Class<?> cls, String expression, boolean recursive, Object... params) throws Exception {
+    // 静态类开始的表达式执行
+    public static <T> T withStatic(Class<?> cls, String expression, boolean recursive, Object... params)
+            throws ReflectionException, EvaluateException {
         // noinspection unchecked
         return (T) Expression.create(cls, expression, recursive, params).evaluate(null, params);
     }
 
-    public static <T> T withStatic(String cls, String expression, boolean recursive, Object... params) throws Exception {
-        return withStatic(Class.forName(cls), expression, recursive, params);
+    public static <T> T withStatic(String cls, String expression, boolean recursive, Object... params)
+            throws ReflectionException, EvaluateException {
+        try {
+            return withStatic(Class.forName(cls), expression, recursive, params);
+        } catch (ClassNotFoundException e) {
+            throw new ReflectionException("cannot find class " + cls, e);
+        }
     }
 
     // 实例开始的表达式执行
-    public static <T> T withInstance(Object instance, String expression, boolean recursive, Object... params) throws Exception {
+    public static <T> T withInstance(Object instance, String expression, boolean recursive, Object... params)
+            throws ReflectionException, EvaluateException {
         // noinspection unchecked
         return (T) Expression.create(instance.getClass(), expression, recursive, params).evaluate(instance, params);
     }
